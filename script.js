7ec7cd6a42771c1ceb92c7175b5d1732a485cde4
Anirudh4583 <201951024@iiitvadodara.ac.in>
2021-04-15 20:06:06 +0530

    added javascript functionalty for taking input

--- script.js
+++ script.js
@@ -1,34 +1,67 @@
 backgroundstate();
 
 function backgroundstate() {
-    let bgWork = localStorage.getItem('bgWork');
+  let bgWork = localStorage.getItem('bgWork');
 
-    // if the script is working in the backgroung
-    if (bgWork === 1) {
-        // show plan icon and hide plane slash icon
-        document.querySelector('.fa-plane-slash').style.visibility = 'hidden';
-        document.querySelector('.fa-plane').style.visibility = 'visible';
-        // disable input field
-        document.querySelector('.inputVal').disabled = true;
-
-    } else if (bgWork === 0) {
-        // show plan icon and hide plane slash icon
-        document.querySelector('.fa-plane-slash').style.visibility = 'visible';
-        document.querySelector('.fa-plane').style.visibility = 'hidden';
-        // disable input field
-        document.querySelector('.inputVal').value = '';
-    }
+  // if the script is working in the backgroung
+  if (bgWork === 1) {
+    // show plan icon and hide plane slash icon
+    document.querySelector('.fa-plane-slash').style.visibility = 'hidden';
+    document.querySelector('.fa-plane').style.visibility = 'visible';
+    // disable input field
+    document.querySelector('.inputVal').disabled = true;
+  } else if (bgWork === 0) {
+    // show plan icon and hide plane slash icon
+    document.querySelector('.fa-plane-slash').style.visibility = 'visible';
+    document.querySelector('.fa-plane').style.visibility = 'hidden';
+    // disable input field
+    document.querySelector('.inputVal').value = '';
+  }
 }
 
 document.querySelector('.fa-check').addEventListener('click', () => {
-    let value = document.querySelector('.inputVal').value;
+  let obj = {
+    value: document.querySelector('.inputVal').value,
+  };
 
-    if (value != '') {
-        if (value < 0) {
-            document.querySelector(".inputvalue").style.border = "2px solid red";
-            setTimeout(() => {
-                document.querySelector(".inputvalue").style.border = "1px solid black";
-            }, 2500);
-        }
+  if (obj.value != '') {
+    if (obj.value < 0) {
+      document.querySelector('.inputvalue').style.border = '2px solid red';
+      setTimeout(() => {
+        document.querySelector('.inputvalue').style.border = '1px solid black';
+      }, 2500);
+    } else {
+      localStorage.setItem('bgWork', 1);
+      document.querySelector('inputVal').disabled = true;
+      // localStorage.setItem('value', obj.value);
+      // localStorage.setItem('area', obj.select);
+      document.querySelector('.fa-plane-slash').style.visibility = 'hidden';
+      document.querySelector('.fa-plane').style.visibility = 'visible';
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {
+          message: 'checkClicked',
+          object: obj,
+        });
+      });
     }
+  } else {
+    console.log('enter value');
+    document.querySelector('inputVal').style.border = '2px solid red';
+    setTimeout(() => {
+      document.querySelector('inputVal').style.border = '1px solid black';
+    }, 2000);
+  }
+});
+
+document.querySelector('.fa-times').addEventListener('click', () => {
+  document.querySelector('.inputVal').disabled = false;
+  document.querySelector('.inputVal').value = '';
+  localStorage.setItem('bgWork', 0);
+  document.querySelector('.fa-plane-slash').style.visibility = 'visible';
+  document.querySelector('.fa-plane').style.visibility = 'hidden';
+  chrome.tabs.query({ currentWsindow: true, actise: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { message: 'stopbtnclicked' });
+  });
 });
